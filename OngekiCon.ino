755819314960ca533c7edceaa098e13af18613fd
Moldypie <48046412+Moldypie@users.noreply.github.com>
2020-04-07 03:53:29 -0500

    Update OngekiCon.ino

--- OngekiCon.ino
+++ OngekiCon.ino
@@ -2,7 +2,6 @@
  * 
  * Before you can use this code, make sure to download needed libraries
  * - https://github.com/dmadison/ArduinoXInput
- * - https://github.com/PaulStoffregen/Encoder
  * - https://github.com/FastLED/FastLED
  * 
  * You will also need to add this to your arduino IDE's hardware folder
@@ -16,16 +15,15 @@
  * ONGEKI's XInput lever is processed as when neither trigger is pressed, your character is in the center of the screen.
  * Press the left side and it moves left, release and it returns to center. Same with the right side
  * 
- * lever is done here using an encoder with a 0 value of straight up. +-87 each direction
- * you will need to have the lever straight up upon each boot of the controller, later I'll do this with a potentiometer so we can avoid this...but for now, good enough.
- * the buttons are standard emulated XInput buttons. Test/Service are still keyboard binds with ONGEKI, so I did not include those buttons
+ * My lever is done using a potentiometer that has a range of 15-1023, so you may need to edit the numbers I used.
+ * I set the lever range to 513 which is an approximate halfway point.
+ * I also set the lever calculations to work around that half way point.
  * 
- * Lights use a ws2912b strip. This currently only has 12 lights programmed. 3 for each side button and 2 sets of button lights
+ * Lights use a ws2812b strip. This currently only has 12 lights programmed. 3 for each side button and 2 sets of button lights
  * the lights are set to always be on as segatools does not yet support HID for ONGEKI
  */
 
 #include <XInput.h>
-#include <Encoder.h>
 #include <FastLED.h>
 
 // LED Strip Setup
@@ -34,13 +32,12 @@
 CRGB leds[NUM_LEDS];
 
 // Lever Setup
-float lever = 0;
-float old_lever = 0;
-float positionL = 0;
-float positionR = 0;
+int leverRange = 513;
+int positionL = 0;
+int positionR = 0;
 
 // Lever Pins
-Encoder enc(0, 1);
+const int Pin_Lever = A0;
 
 // Button Pins
 const int Pin_LeftA = 2;
@@ -58,8 +55,8 @@ void setup() {
   // LED Setup
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
 
-  // Lever Setup
-  XInput.setTriggerRange(0, 87);
+  // Set Lever Range
+  XInput.setTriggerRange(0, leverRange);
   
   // Set buttons as inputs, using internal pull-up resistors
   pinMode(Pin_LeftA, INPUT_PULLUP);
@@ -78,7 +75,7 @@ void setup() {
 
 void loop() {
   // Lever Setup
-  lever = (float)(enc.read());
+  //lever = (float)(enc.read());
 
   // LED Colors
   for(int i = 0; i < 3; ++i){
@@ -96,6 +93,7 @@ void loop() {
   FastLED.show();
   
 	// Read pin values and store in variables
+  int lever = analogRead(Pin_Lever);
   boolean leftA = !digitalRead(Pin_LeftA);
   boolean leftB = !digitalRead(Pin_LeftB);
   boolean leftC = !digitalRead(Pin_LeftC);
@@ -124,31 +122,10 @@ void loop() {
   XInput.setTrigger(TRIGGER_RIGHT, positionL);
 
   // Calculate lever values
-  if(lever != old_lever){
-    // if there's a difference in encoder movement from last pass, move the mouse
-    if(lever < old_lever){
-      if(lever < 0){
-      positionR = -lever;
-      }else{
-      positionL = lever;
-      }
-    }else{
-      if(lever > 0){
-      positionL = lever;
-      }else{
-      positionR = -lever;
-      }
-    }
- // we count the difference in the encoders, but we must not go over arduino's int limit
-    if(lever < -255){
-      enc.write(0);
-      old_lever = 0;
-    }else if (lever > 255){
-      enc.write(0);
-      old_lever = 0;
-    }else{
-      old_lever = lever;
-    }
+  if(lever < 513){
+    positionR = -(lever-leverRange);
+  }else{
+    positionL = lever-leverRange;
   }
   
 	// Send control data to the computer
